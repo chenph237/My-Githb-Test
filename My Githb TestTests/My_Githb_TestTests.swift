a70ad4b4e1d332fb398a98beb6cee0d98f10215e
陳炳宏 <chenph@chenbinghongdeimac.local>
2018-04-26 09:48:01 +0800

    Initial Commit

---
+++ My Githb TestTests/My_Githb_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  My_Githb_TestTests.swift
+//  My Githb TestTests
+//
+//  Created by 陳炳宏 on 2018/4/26.
+//  Copyright © 2018年 pencil. All rights reserved.
+//
+
+import XCTest
+@testable import My_Githb_Test
+
+class My_Githb_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
